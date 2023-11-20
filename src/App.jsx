70c0851791aeba60fc4440bdaed91b19a1ec4863
RebecaOlive <oliveiraferreirarebeca@hotmail.com>
2023-11-19 23:31:11 -0300

    moveu imagens da pasta public e componentizou os componentes menu e cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import Cadastro from './components/Cadastro';
+import Cadastro from './components/cadastro/Cadastro';
 
 function App() {
   const [count, setCount] = useState(0);
