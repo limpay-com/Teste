c9fd1e794d239832b86cd24953963c8a088accf0
Silvio Gabriel <silviogabrielgsantana@gmail.com>
2023-12-08 20:21:49 -0300

    Fazendo card de perfil e carrossel

--- src/components/cardGrid/CardGrid.jsx
+++ src/components/cardGrid/CardGrid.jsx
@@ -1,20 +1,103 @@
+import { grep } from 'jquery';
 import CardPerfil from '../cardPerfil/CardPerfil';
+import './CardGrid.css'
+import { motion } from 'framer-motion'
+import { useState,useEffect, useRef } from 'react';
 
 function CardGrid() {
+
+  const usuarios = [<CardPerfil/> ,<CardPerfil/>, <CardPerfil/>, <CardPerfil/>, <CardPerfil/> ]
+
+  const carousel = useRef();
+  const [width , setWidth] = useState(0)
+  
+  useEffect(() => {
+    console.log(carousel.current?.scrollWidth, carousel.current?.offsetWidth)
+    setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth)
+  }, [])
+
   return (
-    <div>
-      <div className="row">
-        <div className="col-sm">
-          <CardPerfil />
-        </div>
-        <div className="col-sm">
-          <CardPerfil />
-        </div>
-        <div className="col-sm">
-          <CardPerfil />
-        </div>
+
+
+    <> 
+
+      <motion.div ref={carousel} className='carousel' whileTap={{cursor: "grabbing"}}>
+        <motion.div className='inner'
+        drag="x"
+        dragConstraints={{ right: 0 , left: -width}}
+        >
+
+          {usuarios.map(usuario => (
+            <motion.div className="cards-wrapper" key={usuario}>
+              {usuario}
+            </motion.div>
+          ))}
+
+        </motion.div>
+      </motion.div>
+
+    
+    </>
+    // <div>
+    //   <div className="row">
+    //     <div className="col-sm">
+    //       <CardPerfil nameUsuario="Silvio" localizacao="Guarulhos - SP" avaliacao="5.0"/>
+    //     </div>
+    //     <div className="col-sm">
+    //     <CardPerfil nameUsuario="Rebeca" localizacao="Grajau - SP" avaliacao="5.0"/>
+    //     </div>
+    //     <div className="col-sm">
+    //     <CardPerfil nameUsuario="Alexandre Lemes" localizacao="Mogi - SP" avaliacao="5.0"/>
+    //     </div>
+    //   </div>
+    // </div> -------------------------------------------
+
+    /* <div id="carouselExampleControls" class="carousel slide " data-bs-ride="carousel">
+  <div class="carousel-inner ">
+    <div class="carousel-item active ">
+      <div className="cards-wrapper">
+      <CardPerfil></CardPerfil>
       </div>
     </div>
+    <div class="carousel-item ">
+    <div className="cards-wrapper">
+      <CardPerfil></CardPerfil>
+      </div>
+    </div>
+    <div class="carousel-item ">
+    <div className="cards-wrapper">
+      <CardPerfil></CardPerfil>
+      </div>
+    </div>
+    <div class="carousel-item ">
+    <div className="cards-wrapper">
+      <CardPerfil></CardPerfil>
+      </div>
+    </div>
+    <div class="carousel-item ">
+    <div className="cards-wrapper">
+      <CardPerfil></CardPerfil>
+      </div>
+    </div>
+    <div class="carousel-item ">
+    <div className="cards-wrapper">
+      <CardPerfil></CardPerfil>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon bg-black" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon bg-black" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div> -----------------------------------------*/
+
+
+
+
   );
 }
 
--- src/components/cardPerfil/CardPerfil.jsx
+++ src/components/cardPerfil/CardPerfil.jsx
@@ -1,52 +1,77 @@
 import React from 'react';
 import './CardPerfil.css';
+import Fotomulher from '../../../src/assets/images/cardPerfil/exemplo.jpg'
+import ChevronRight from '../../../src/assets/images/cardPerfil/chevron-right.svg'
+import Star from '../../../src/assets/images/cardPerfil/Star1.svg'
 
 const CardPerfil = () => {
+
   return (
-    <div className="mt-4">
-      <div className="row justify-content-between">
-        <div className="col-md mb-4">
-          <div className="card">
-            <div className="color"></div>
-            <div className="img1">
-              <img src="./src/assets/images/cardPerfil/exemplo.jpg" alt="" />
-            </div>
-            <div className="main">
-              <h2>Maria Silva</h2>
-              <div className="container text-center">
-                <button
-                  type="button"
-                  className="btn btn-success btn-xs customizado-botao"
-                >
-                  Cozinha
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-success btn-xs customizado-botao"
-                >
-                  Lavanderia
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-success btn-xs customizado-botao"
-                >
-                  Cozinha
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-success btn-xs customizado-botao"
-                >
-                  Sala de Jantar
-                </button>
-                <a href="http:// " className="mb-4">
-                  Mais informações
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div>
+      {/* style={{width: 300}} */}
+    <div class="card card-profile " >
+  <div className='color'>
+      <div className="img1">
+          <img className='img-fluid img-thumbnail background' src={Fotomulher} alt="" />
       </div>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Silvio <img src={ChevronRight} style={{width: 30 ,height: 30}} /></h5>
+    <p class="card-text-localizacao">Lapa - SP</p>
+    <p class="card-text-preco">R$100 - 200,00 dia  <span><img className='mx-2 mb-1' src={Star}/>5.0</span></p>
+    <div className='d-flex align-items-start align-content-start gap-1 flex-wrap w-100 mt-2'>
+      <a href="#" class="d-inline bg-light customizado-botao">Cozinha</a>
+      <a href="#" class="d-inline bg-light customizado-botao">sala de estar</a>
+      <a href="#" class="d-inline bg-light customizado-botao">Quarto</a>
+      <a href="#" class="d-inline bg-light customizado-botao">Quarto</a>
+      <a href="#" class="d-inline bg-light customizado-botao">Quarto</a>
+      <a href="#" class="d-inline bg-light customizado-botao">Banheiro</a>
     </div>
+  </div>
+</div>
+
+
+    
+    </div>
+    // <div className="mt-4">
+    //   {/* <div className="row justify-content-between">
+    //     <div className="col-md mb-4">
+    //       <div className="card">
+    //         <div className="color"></div>
+    //         <div className="img1">
+    //           <img src="./src/assets/images/cardPerfil/exemplo.jpg" alt="" />
+    //         </div>
+    //         <div className="main d-flex flex-column gap-3">
+    //           <h2>Maria Silva</h2>
+    //           <div className="text-center d-flex container-filtros flex-wrap">
+    //             <a
+    //               className="d-inline bg-light customizado-botao"
+    //             >
+    //               Cozinha
+    //             </a>
+    //             <a
+    //               className="d-inline bg-light customizado-botao"
+    //             >
+    //               Lavanderia
+    //             </a>
+    //             <a
+    //               className="d-inline bg-light customizado-botao"
+    //             >
+    //               Cozinha
+    //             </a>
+    //             <a
+    //               className="d-inline bg-light customizado-botao"
+    //             >
+    //               Sala de Jantar
+    //             </a>
+                
+
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div> */}
+    // </div>
   );
 };
 
