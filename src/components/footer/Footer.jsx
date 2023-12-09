ef8dc5d9a19cb79d7d121039e5f10594b0718c36
stephanymdias <stephanystudies1@gmail.com>
2023-12-08 23:02:03 -0300

    Fazendo alterações no Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,88 +1,63 @@
-import React from "react";
-import './footer.css';
-import logoFooter from "../../assets/images/logo-branca.svg";
-import { IoLogoGithub } from "react-icons/io";
-import { FaFacebook } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaYoutube } from "react-icons/fa";
+import React from 'react';
+import './Footer.css'; // Estilos CSS
 
 const Footer = () => {
-    return (
-        <div className="footer">
-            <div className="sb__footer section__padding">
-                <div className="sb__footer-links">
-                    <div id="divLogoFooter" className="sb__footer-links_div">
-                        <img src= {logoFooter} alt="" />
-                        </div>
-
-                    {/* sobre a limpay */}
-                    <div className="sb__footer-links_div">
-                        <h4>Sobre a Limpay</h4>
-                        <a href="/quemsomos">
-                            <p>Quem somos</p>
-                        </a>
-                        <a href="/missao-visao-e-valores">
-                            <p>Missão, Visão e Valores</p>
-                        </a>
-                        <a href="/termos">
-                            <p>Termos de Serviço</p>
-                        </a>
-                        <a href="/privacidade">
-                            <p>Política de Privacidade</p>
-                        </a>
-                    </div>
-
-                    {/* serviços */}
-                    <div className="sb__footer-links_div">
-                        <h4>Serviços</h4>
-                        <a href="/contratacao">
-                            <p>Contratação</p>
-                        </a>
-                        <a href="/perfil">
-                            <p>Perfil</p>
-                        </a>
-                        <a href="/carteira">
-                            <p>Carteira</p>
-                        </a>
-                        <a href="/educacional">
-                            <p>Educacional</p>
-                        </a>
-                    </div>
-
-                    {/* suporte */}
-                    <div className="sb__footer-links_div">
-                        <h4>Suporte</h4>
-                        <a href="/tutorial">
-                            <p>Tutorial</p>
-                        </a>
-                        <a href="/ajuda">
-                            <p>Centro de Ajuda</p>
-                        </a>
-                    </div>
-
-                    <div className="sb__footer-links_div">
-                        <h4>Siga as nossas redes sociais:</h4>
-                        <div className="socialmedia">
-                            <p><IoLogoGithub /></p>
-                            <p><FaFacebook /></p>
-                            <p><FaLinkedin /></p>
-                            <p><FaInstagram /></p>
-                            <p><FaYoutube /></p>
-                        </div>
-                    </div>
-                </div>
-
-                <hr />
-
-                <div className="sb__footer-below">
-                    <div className="sb__footer-copyright">
-                        <p>@{new Date().getFullYear()} Todos os direitos reservados.</p>
-                    </div>
-                </div>
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3">
+            {/* Coluna para a logo */}
+            <img src="caminho/para/sua/logo.png" alt="Logo" className="footer-logo" />
+          </div>
+          <div className="col-md-6">
+            {/* Colunas: Sobre, Serviços, Suporte */}
+            <div className="row">
+              <div className="col-md-4">
+                <h4>Sobre</h4>
+                <ul>
+                  <li><a href="#">Quem somos</a></li>
+                  <li><a href="#">Missão, Visão e Valores</a></li>
+                  <li><a href="#">Termos de Serviço</a></li>
+                  <li><a href="#">Política de Privacidade</a></li>
+                </ul>
+              </div>
+              <div className="col-md-4">
+                <h4>Serviços</h4>
+                <ul>
+                  <li><a href="#">Contratação</a></li>
+                  <li><a href="#">Perfil</a></li>
+                  <li><a href="#">Carteira</a></li>
+                  <li><a href="#">Educacional</a></li>
+                </ul>
+              </div>
+              <div className="col-md-4">
+                <h4>Suporte</h4>
+                <ul>
+                  <li><a href="#">Tutorial</a></li>
+                  <li><a href="#">Centro de Ajuda</a></li>
+                </ul>
+              </div>
             </div>
+          </div>
+          <div className="col-md-3">
+            {/* Coluna: Siga nossas redes sociais */}
+            <div className="footer-social">
+              <h4>Siga as nossas redes sociais:</h4>
+              {/* Adicione aqui os ícones das redes sociais */}
+            </div>
+          </div>
         </div>
-    );
+        <hr />
+        <div className="row">
+          <div className="col-md-12 text-center">
+            {/* Copyright */}
+            <p>@{new Date().getFullYear()} Todos os direitos reservados.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
